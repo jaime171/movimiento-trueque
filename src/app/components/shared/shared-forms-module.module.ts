9a17438f78bcf3b8e8174c93e46d7b6e27fe3bfd
jaime <jaimesalvador.simental@gmail.com>
2020-04-16 00:46:42 -0500

    dialog added

--- src/app/components/shared/shared-forms-module.module.ts
+++ src/app/components/shared/shared-forms-module.module.ts
@@ -4,17 +4,26 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [],
-  imports: [],
+  declarations: [DialogComponent],
+  entryComponents: [DialogComponent],
+  imports: [
+    MatFormFieldModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule
+  ],
   exports: [
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    DialogComponent
   ]
 })
 export class SharedFormsModule { }
---
+++ src/app/components/shared/shared.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  constructor() { }
+
+  public feeds = [
+    {
+      id: 1,
+      name: "Reparación de tuberías",
+      date: "2 h",
+      description: "Servicio de Detección, Reparación, Mantenimiento y más. Tecnicos Altamente Capacitados, Soluciones Definitivas. Plomeros y Electricistas Urgentes.",
+      stock: 3,
+      thumbnailImg: "user_1.jpg",
+      serviceImage: "service_1.jpg",
+      endDate: "10/12/2020",
+      type: "servicio",
+      author: "Laura Perez",
+      coverImg: "cover.jpg"
+    },
+    {
+      id: 2,
+      name: "Fruteria De la Rosa",
+      date: "1 h",
+      description: "El delicioso antojo saludable",
+      stock: 2,
+      thumbnailImg: "user_2.jpg",
+      serviceImage: "service_2.jpg",
+      endDate: "8/12/2020",
+      type: "producto",
+      author: "Juan Perez",
+      coverImg: "cover.jpg"
+    },
+    {
+      id: 3,
+      name: "Comida para lleva la Costeña",
+      date: "5 h",
+      description: "Cabrito, Arrachera, Agujas y Tablitas. Casa Fundadora de la Auténtica Cocina Norestense.",
+      stock: 3,
+      thumbnailImg: "user_1.jpg",
+      serviceImage: "service_3.jpg",
+      endDate: "10/11/2020",
+      type: "producto",
+      author: "Laura Rosas",
+      coverImg: "cover.jpg"
+    },
+    {
+      id: 4,
+      name: "Arquitectura Ramirez",
+      date: "1 h",
+      description: "Ganarse la confianza del cliente sin importar las dimensiones del proyecto a realizar es indispensable, porque tan importante es el que quiere construir un muro como el que quiere construir un edificio.",
+      stock: 2,
+      thumbnailImg: "user_2.jpg",
+      serviceImage: "service_4.jpg",
+      endDate: "10/10/2020",
+      type: "servicio",
+      author: "Miguel Martinez",
+      coverImg: "cover.jpg"
+    }
+  ];
+}
